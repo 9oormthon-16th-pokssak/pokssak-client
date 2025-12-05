049cb0df760627cc7b2adba7d89ffd05e5e041fe
hjunieee <hinara12@inu.ac.kr>
2025-12-05 10:41:11 +0900

    fix: 토큰 안들어가는 문제 수정

--- src/apis/core.ts
+++ src/apis/core.ts
@@ -4,8 +4,16 @@ const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json; charset=utf-8",
-    Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
   },
+});
+
+// 요청 인터셉터 설정
+apiClient.interceptors.request.use(config => {
+  const token = localStorage.getItem("accessToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 // 공통 API 응답 구조
